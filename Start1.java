047d1dfbaeb1a048c2243a9aea7e27ec55fc526c
HaoDang181 <haodang1810@gmail.com>
2022-07-08 15:16:07 +0700

    Okay

---
+++ Start1.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Start1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(4);
+        for(int i : list){
+            System.out.println(i);
+        }
+    }
+}
